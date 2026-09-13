eb7c5661ecd78b7cc46fc3529ed1c636f86fffa8
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-09-13 18:42:51 +0100

    feat: implement HTTP handlers for user registration and login

---
+++ internal/user/handler.go
@@ -0,0 +1,49 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	svc *Service
+}
+
+func NewHandler(s *Service) *Handler {
+	return &Handler{svc: s}
+}
+
+func (h *Handler) Register(c *gin.Context) {
+	var input RegisterRequest
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	authResponse, err := h.svc.Register(c, input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully", "data": authResponse})
+}
+
+func (h *Handler) Login(c *gin.Context) {
+	var input LoginRequest
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	authResponse, err := h.svc.Login(c, input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged in successfully", "data": authResponse})
+}
