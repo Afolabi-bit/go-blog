70edd204fe35d2a7c779b8565e2c1157626fa72f
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-09-13 17:35:51 +0100

    Created the Register busines logic

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,5 +1,16 @@
 package user
 
+import (
+	"blog-api/internal/auth"
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type Service struct {
 	repo      *Repo
 	jwtSecret string
@@ -10,4 +21,72 @@ func NewService(repo *Repo, jwtSecret string) *Service {
 		repo:      repo,
 		jwtSecret: jwtSecret,
 	}
+}
+
+func (s *Service) Register(ctx context.Context, input RegisterRequest) (AuthResponse, error) {
+	email := input.Email
+	password := input.Password
+	firstName := input.FirstName
+	lastName := input.LastName
+
+	if email == "" {
+		return AuthResponse{}, fmt.Errorf("Email is required")
+	}
+	if firstName == "" {
+		return AuthResponse{}, fmt.Errorf("First name is required")
+	}
+	if lastName == "" {
+		return AuthResponse{}, fmt.Errorf("Last name is required")
+	}
+	if password == "" {
+		return AuthResponse{}, fmt.Errorf("Password is required")
+	}
+	if len(password) < 6 {
+		return AuthResponse{}, fmt.Errorf("Password must be at least 6 characters")
+	}
+
+	_, err := s.repo.FindUserByEmail(ctx, email)
+
+	if err == nil {
+		return AuthResponse{}, fmt.Errorf("Email already registered!")
+	}
+
+	if errors.Is(mongo.ErrNoDocuments, err) {
+		return AuthResponse{}, err
+	}
+
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return AuthResponse{}, fmt.Errorf("Failed to hash password: %w", err)
+	}
+
+	now := time.Now()
+
+	user := User{
+		Email:        email,
+		Role:         RoleReader,
+		FirstName:    firstName,
+		LastName:     lastName,
+		PasswordHash: string(hashedBytes),
+		CreatedAt:    now,
+		UpdatedAt:    now,
+	}
+
+	createdUser, err := s.repo.CreateUser(ctx, user)
+
+	if err != nil {
+		return AuthResponse{}, err
+	}
+
+	token, err := auth.CreateToken(s.jwtSecret, createdUser.ID.Hex(), createdUser.Email, createdUser.Role)
+
+	if err != nil {
+		return AuthResponse{}, err
+	}
+
+	return AuthResponse{
+		Token: token,
+		User:  ToPublic(createdUser),
+	}, nil
 }
