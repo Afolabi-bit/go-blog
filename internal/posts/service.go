dcbe6e28b11cc409354c7af6a0af4d97188fd9c9
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-09-16 16:58:39 +0100

    feat: implement posts service with slug generation logic

--- internal/posts/service.go
+++ internal/posts/service.go
@@ -3,7 +3,13 @@ package posts
 import (
 	"blog-api/internal/user"
 	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
 )
+
+var slugRegex = regexp.MustCompile(`[^a-z0-9]+`)
 
 var (
 	ErrNotFound     = errors.New("post not found")
@@ -21,4 +27,20 @@ func NewService(repo *Repo, user *user.Repo) *Service {
 		repo: repo,
 		user: user,
 	}
+}
+
+func generateSlug(title string) string {
+	title = strings.ToLower(strings.TrimSpace(title))
+
+	reg := regexp.MustCompile(`[^a-z0-9]+`)
+
+	slug := reg.ReplaceAllString(title, "-")
+
+	slug = strings.Trim(slug, "-")
+
+	if slug == "" {
+		slug = fmt.Sprintf("p-w-t-%d", time.Now().Unix())
+	}
+
+	return slug
 }
