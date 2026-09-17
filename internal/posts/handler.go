f85123c3e26c05c74109041933f1620261fb8b28
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-09-17 09:25:46 +0100

    feat(posts): add err handler

--- internal/posts/handler.go
+++ internal/posts/handler.go
@@ -1,5 +1,13 @@
 package posts
 
+import (
+	"blog-api/internal/response"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Handler struct {
 	svc *Service
 }
@@ -7,5 +15,18 @@ type Handler struct {
 func NewHandler(s *Service) *Handler {
 	return &Handler{
 		svc: s,
+	}
+}
+
+func (h *Handler) handleError(c *gin.Context, err error) {
+	switch {
+	case errors.Is(err, ErrNotFound):
+		response.Error(c, http.StatusNotFound, err.Error())
+	case errors.Is(err, ErrForbidden):
+		response.Error(c, http.StatusForbidden, err.Error())
+	case errors.Is(err, ErrInvalidInput):
+		response.Error(c, http.StatusBadRequest, err.Error())
+	default:
+		response.Error(c, http.StatusInternalServerError, "internal server error")
 	}
 }
