b687cea8cb6f6b6f0c18df009e1ff784a9534b1c
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-09-17 09:26:36 +0100

    feat: implement posts reation HTTP handler

--- internal/posts/handler.go
+++ internal/posts/handler.go
@@ -1,6 +1,7 @@
 package posts
 
 import (
+	"blog-api/internal/middleware"
 	"blog-api/internal/response"
 	"errors"
 	"net/http"
@@ -29,4 +30,36 @@ func (h *Handler) handleError(c *gin.Context, err error) {
 	default:
 		response.Error(c, http.StatusInternalServerError, "internal server error")
 	}
+}
+
+func (h *Handler) CreatePost(c *gin.Context) {
+	userID, ok := middleware.GetUserID(c)
+
+	if !ok {
+		response.Error(c, http.StatusUnauthorized, "Please login to create post.")
+		return
+	}
+
+	user, err := h.svc.user.FinduserByID(c, userID)
+	if err != nil {
+		h.handleError(c, err)
+		return
+	}
+
+	var post CreatePostRequest
+
+	if err = c.ShouldBindJSON(&post); err != nil {
+		response.Error(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	authorName := user.FirstName + " " + user.LastName
+	createdPost, err := h.svc.CreateNewPost(c, user.ID, authorName, post)
+
+	if err != nil {
+		h.handleError(c, err)
+		return
+	}
+
+	response.Success(c, http.StatusCreated, "post successfully created", createdPost)
 }
